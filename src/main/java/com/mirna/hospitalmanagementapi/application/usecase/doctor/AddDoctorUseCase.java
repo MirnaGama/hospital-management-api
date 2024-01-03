ef0424761849934e82046c5cfefb2458219da6b2
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-03 19:03:06 -0300

    [R1] Add doctor use case

---
+++ src/main/java/com/mirna/hospitalmanagementapi/application/usecase/doctor/AddDoctorUseCase.java
@@ -0,0 +1,31 @@
+package com.mirna.hospitalmanagementapi.application.usecase.doctor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mirna.hospitalmanagementapi.domain.entities.Doctor;
+import com.mirna.hospitalmanagementapi.domain.repositories.DoctorRepository;
+
+/**
+ * This class is used to execute the save method from doctor repository
+ * 
+ * @author Mirna Gama
+ * @version 1.0
+ */
+@Component
+public class AddDoctorUseCase {
+
+	@Autowired
+	private DoctorRepository doctorRepository;
+	
+	/**
+	 * Executes the save method from Doctor repository
+	 * 
+	 * @param doctor The Doctor to be saved in the repository
+	 * @return The saved doctor if successful, or null if there is an error
+	 *
+	 */
+	public Doctor execute(Doctor doctor) {
+        return this.doctorRepository.save(doctor);
+    }
+}
