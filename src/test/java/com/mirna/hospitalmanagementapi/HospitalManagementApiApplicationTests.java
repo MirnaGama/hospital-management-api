fb07608e719bc6bd5c8311475f110ff6076104c7
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-03 18:01:17 -0300

    Spring Initializr

---
+++ src/test/java/com/mirna/hospitalmanagementapi/HospitalManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirna.hospitalmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
