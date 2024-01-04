7fd733d36e6f84f697dd9d78bec318c8346428d6
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-04 15:49:49 -0300

    [R1] Add active profile for test

--- src/test/java/com/mirna/hospitalmanagementapi/unit/application/controllers/DoctorControllerTest.java
+++ src/test/java/com/mirna/hospitalmanagementapi/unit/application/controllers/DoctorControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -25,6 +26,7 @@ import com.mirna.hospitalmanagementapi.domain.enums.Specialty;
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 public class DoctorControllerTest {
 
 	@Autowired
