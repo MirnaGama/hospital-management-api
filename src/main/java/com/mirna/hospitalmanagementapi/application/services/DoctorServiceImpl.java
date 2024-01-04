09dcab46707472ad5db8eb5a6ecc37951f317e45
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-04 16:59:06 -0300

    [R2] Add find method in service class

--- src/main/java/com/mirna/hospitalmanagementapi/application/services/DoctorServiceImpl.java
+++ src/main/java/com/mirna/hospitalmanagementapi/application/services/DoctorServiceImpl.java
@@ -1,10 +1,14 @@
 package com.mirna.hospitalmanagementapi.application.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mirna.hospitalmanagementapi.application.usecase.doctor.AddDoctorUseCase;
+import com.mirna.hospitalmanagementapi.application.usecase.doctor.FindAllDoctorsUseCase;
 import com.mirna.hospitalmanagementapi.domain.dtos.DoctorDTO;
+import com.mirna.hospitalmanagementapi.domain.dtos.DoctorPublicDataDTO;
 import com.mirna.hospitalmanagementapi.domain.entities.Doctor;
 import com.mirna.hospitalmanagementapi.domain.services.DoctorService;
 
@@ -23,6 +27,9 @@ public class DoctorServiceImpl implements DoctorService {
 	@Autowired
 	private AddDoctorUseCase addDoctor;
 	
+	@Autowired
+	private FindAllDoctorsUseCase findAllDoctors;
+	
 	/**
 	 * Adds a new doctor to the database.
 	 *
@@ -35,6 +42,16 @@ public class DoctorServiceImpl implements DoctorService {
 		Doctor doctor = new Doctor(doctorDTO);
 		
 		return addDoctor.execute(doctor);
+	}
+
+	/**
+	 * Finds doctors from the database.
+	 *
+	 * @return A paginated list containing data transfer objects with doctors public information in the repository
+	 */
+	@Override
+	public List<DoctorPublicDataDTO> findDoctors() {
+		return findAllDoctors.execute().stream().map(DoctorPublicDataDTO::new).toList();
 	}
 
 }
