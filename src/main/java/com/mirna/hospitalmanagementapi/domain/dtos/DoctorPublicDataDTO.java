9d3bfcf6da0a4003bedbce8e9c4fa07d5bee4b1a
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-04 18:09:06 -0300

    [R2] Add java doc

--- src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/DoctorPublicDataDTO.java
+++ src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/DoctorPublicDataDTO.java
@@ -3,6 +3,12 @@ package com.mirna.hospitalmanagementapi.domain.dtos;
 import com.mirna.hospitalmanagementapi.domain.entities.Doctor;
 import com.mirna.hospitalmanagementapi.domain.enums.Specialty;
 
+/**
+* Data transfer object used to format and display only the public data allowed for Doctor entity
+* 
+* @author Mirna Gama
+* @version 1.0
+*/
 public record DoctorPublicDataDTO(String name, String email, String crm, Specialty specialty){
 	
 	public DoctorPublicDataDTO(Doctor doctor){
