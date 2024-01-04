104c5377910ffc459f38c68ee3476e20a39806c4
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-04 10:16:10 -0300

    [R1] Add validation annotation for address and doctor dtos

--- src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/DoctorDTO.java
+++ src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/DoctorDTO.java
@@ -2,6 +2,34 @@ package com.mirna.hospitalmanagementapi.domain.dtos;
 
 import com.mirna.hospitalmanagementapi.domain.enums.Speciality;
 
-public record DoctorDTO(String name, String email, String crm, String telephone, Speciality speciality, AddressDTO addressDTO) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DoctorDTO(
+		
+		@NotBlank(message="name cannot be blank")
+		String name, 
+		
+		@NotBlank(message="email cannot be blank")
+		@Email(message="invalid format for email")
+		String email, 
+		
+		@NotBlank(message="crm cannot be blank")
+		@Pattern(regexp = "\\d{4,6}", message="invalid format for crm")
+		String crm, 
+		
+		@NotBlank(message="telephone cannot be blank")
+		String telephone, 
+		
+		@NotNull(message="speciality cannot be null")
+		@Valid
+		Speciality speciality, 
+		
+		@NotNull(message="address cannot be null")
+		@Valid
+		AddressDTO address) {
 
 }
