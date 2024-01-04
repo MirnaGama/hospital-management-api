ed81548b4501ec0d94cfef02c89ee33f154df74c
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-04 10:16:10 -0300

    [R1] Add validation annotation for address and doctor dtos

--- src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/AddressDTO.java
+++ src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/AddressDTO.java
@@ -1,5 +1,27 @@
 package com.mirna.hospitalmanagementapi.domain.dtos;
 
-public record AddressDTO(String street, String neighborhood, String zipCode, String city, String state,
-		String additionalDetails, String houseNumber) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record AddressDTO(
+		
+		@NotBlank(message="street cannot be blank")
+		String street, 
+		
+		@NotBlank(message="neighborhood cannot be blank")
+		String neighborhood, 
+		
+		@NotBlank(message="zipCode cannot be blank")
+		@Pattern(regexp="\\d{8}", message="invalid format for zipCode")
+		String zipCode, 
+		
+		@NotBlank(message="city cannot be blank")
+		String city, 
+		
+		@NotBlank(message="state cannot be blank")
+		String state,
+		
+		String additionalDetails, 
+	
+		String houseNumber) {
 }
