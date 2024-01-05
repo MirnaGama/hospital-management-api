0fc6c9eba879a01776fd7e981343b8b048bfd0c5
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-05 18:40:35 -0300

    [R4] Only find the active doctors

--- src/main/java/com/mirna/hospitalmanagementapi/domain/repositories/DoctorRepository.java
+++ src/main/java/com/mirna/hospitalmanagementapi/domain/repositories/DoctorRepository.java
@@ -1,5 +1,7 @@
 package com.mirna.hospitalmanagementapi.domain.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mirna.hospitalmanagementapi.domain.entities.Doctor;
@@ -12,4 +14,5 @@ import com.mirna.hospitalmanagementapi.domain.entities.Doctor;
 */
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 
+	Page<Doctor> findDoctorsByActiveTrue(Pageable pageable);
 }
