fccdb035a888b409f5ab1d19b846711de06ba723
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-05 12:29:46 -0300

    [R3] DTO to update allowed data in Doctor Entity

---
+++ src/main/java/com/mirna/hospitalmanagementapi/domain/dtos/DoctorUpdatedDataDTO.java
@@ -0,0 +1,18 @@
+package com.mirna.hospitalmanagementapi.domain.dtos;
+
+import jakarta.validation.constraints.NotNull;
+
+/**
+* Data transfer object used to update allowed data in a Doctor entity
+* 
+* @author Mirna Gama
+* @version 1.0
+*/
+public record DoctorUpdatedDataDTO(
+		@NotNull(message="id cannot be null")
+		Long id,
+		String name, 
+		String telephone, 
+		AddressDTO address) {
+
+}
