cddcaf2ab200d9cc1f686b4f86f98a540f18858f
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-11 21:23:57 -0300

    [R9] Get token subject method in Token Service class

--- src/main/java/com/mirna/hospitalmanagementapi/application/services/auth/jwt/TokenServiceImpl.java
+++ src/main/java/com/mirna/hospitalmanagementapi/application/services/auth/jwt/TokenServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mirna.hospitalmanagementapi.application.usecase.auth.jwt.CreateJWTUseCase;
+import com.mirna.hospitalmanagementapi.application.usecase.auth.jwt.GetJWTSubjectUseCase;
 import com.mirna.hospitalmanagementapi.domain.entities.auth.User;
 import com.mirna.hospitalmanagementapi.domain.services.auth.jwt.TokenService;
 
@@ -21,6 +22,9 @@ public class TokenServiceImpl implements TokenService {
 	@Autowired
 	private CreateJWTUseCase createJWT;
 	
+	@Autowired
+	private GetJWTSubjectUseCase getJWTSubject;
+	
 	/**
 	 * Generates the authorization token 
 	 * 
@@ -30,6 +34,17 @@ public class TokenServiceImpl implements TokenService {
 	@Override
 	public String generateToken(User user) {
 		return createJWT.execute(user);
+	}
+
+	/**
+	 * Gets the jwt subject 
+	 * 
+	 * @param token The json web token on request header
+	 * @return A string containing the subject from the decoded jwt
+	 */
+	@Override
+	public String getTokenSubject(String token) {
+		return getJWTSubject.execute(token);
 	}
 	
 }
