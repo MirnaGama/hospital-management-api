06cf0ad5209295e1ad0cb29695c9f9d251de72fd
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-15 16:14:24 -0300

    [R9] H2 database does not accept alter table migration, let's remove it

--- src/main/resources/db/migration/V5__create-table-users.sql
+++ src/main/resources/db/migration/V5__create-table-users.sql
@@ -1,7 +1,7 @@
 create table users (
 
     id bigint not null auto_increment,
-    login varchar(100) not null,
+    login varchar(100) not null unique,
     password varchar(255) not null,
     
     primary key(id)
--- src/main/resources/db/migration/V6__slter-table-modify-column-users.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users
-MODIFY COLUMN login varchar(250) not null unique;
