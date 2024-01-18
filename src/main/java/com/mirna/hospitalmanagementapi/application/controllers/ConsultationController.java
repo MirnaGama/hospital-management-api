84cada51107fb47126e19ea27d175076c18c791e
Mirna Gama <mirnabeatrizgamav@gmail.com>
2024-01-18 15:08:05 -0300

    [R10] Delete method to cancel consultation in controller class

--- src/main/java/com/mirna/hospitalmanagementapi/application/controllers/ConsultationController.java
+++ src/main/java/com/mirna/hospitalmanagementapi/application/controllers/ConsultationController.java
@@ -3,11 +3,13 @@ package com.mirna.hospitalmanagementapi.application.controllers;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mirna.hospitalmanagementapi.domain.dtos.consultation.ConsultationCanceledDTO;
 import com.mirna.hospitalmanagementapi.domain.dtos.consultation.ConsultationDTO;
 import com.mirna.hospitalmanagementapi.domain.exceptions.ConsultationValidationException;
 import com.mirna.hospitalmanagementapi.domain.services.ConsultationService;
@@ -40,5 +42,19 @@ public class ConsultationController {
 	@PostMapping
 	public ResponseEntity<Object> postConsultation(@RequestBody @Valid ConsultationDTO consultationDTO) throws ConsultationValidationException {
 		return ResponseEntity.ok(consultationService.addConsultation(consultationDTO));
+	}
+	
+	/**
+	 * Delete method to update a new Consultation object based on the provided DTO.
+	 *
+	 * @param consultationCanceledDTO The data transfer object containing data to update Consultation
+	 * entity.
+	 * 
+	 * @return A response entity containing the canceled consultation and ok status if successful, or
+	 * a 400-level error if the consultation entity is not found
+	 */
+	@DeleteMapping
+	public ResponseEntity<Object> deleteConsultation(@RequestBody @Valid ConsultationCanceledDTO consultationCanceledDTO) {
+		return ResponseEntity.ok(consultationService.cancelConsultation(consultationCanceledDTO));
 	}
 }
